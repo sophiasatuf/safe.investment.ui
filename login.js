97e0199efa17775b1db5fc22d176f973c59394f8
sophiasatuf <satufsophia@gmail.com>
2023-05-11 15:03:18 -0300

    Adiciona login

--- login.js
+++ login.js
@@ -1,15 +1,23 @@
-function addUser(fullName, age, senha, email, cpf) {
+function addUser(fullName, age, senha, email, cpf, isProfesor) {
 
-    fetch(`http://localhost:6789/user/cadastro?age=${age}&cpf=${cpf}&email=${email}&senha=${senha}&fullName=${fullName}`, {method:"POST"});
+    fetch(`http://localhost:6789/user/cadastro?age=${age}&cpf=${cpf}&email=${email}&senha=${senha}&fullName=${fullName}&isProfessor=${isProfesor}`, {method:"POST"});
 
 }
 
-function loginUser(email, senha) {
-    
-    fetch(`http://localhost:6789/user/login?email=${email}&senha=${senha}`, { method: "POST", mode: 'cors' })
+async function loginUser(email, senha) {
+    let resposta = false;
+    await fetch(`http://localhost:6789/user/login?email=${email}&senha=${senha}`, { method: "POST", mode: 'cors' })
     .then(T => T.text())
-    .then(usuario => {
-      console.log(usuario)
+    .then(IDusuario => {
+      console.log(IDusuario)
+      if(IDusuario == -1) {
+        resposta = false;
+      }
+      else {
+        resposta = true;
+        localStorage.setItem("isLogged", true);
+        localStorage.setItem("IDusuario", IDusuario);
+      }
     })
-
+    return resposta;
 }
